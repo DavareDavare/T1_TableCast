3556b238184823b080524b61c0c5c86975a72c1e
ShizoFX <alessandro.davare@htl-saalfelden.at>
2023-01-23 07:28:16 +0100

    TestWebsite

---
+++ TableCastWebsitePHP/json_store.php
@@ -0,0 +1,21 @@
+<?php
+
+if(isset($_REQUEST['submit']))
+{
+
+
+    $data = '';
+    $filename = "data.json";
+    if(is_file($filename))
+    {
+        $data - file_get_contents($filename);
+    }
+    
+    
+    $json_arr -json_decode($data, true);
+
+    $json_arr[] = array('Helligkeit' => $_REQUEST['Helligkeit'], 'Geschwindigkeit' => $_REQUEST['Geschwindigkeit'], 'Text' => $_REQUEST['Text']);
+    file_put_contents($filename, json_encode($json_arr));
+    header("Location: http://localhost:3000/index.html");
+}
+?>
---
+++ TableCastWebsitePHP/script.js
@@ -0,0 +1,22 @@
+function textarealimit(textarea, rows, chars, rowsleft_id, charsleft_id) {
+    var newValue;
+    var valueSegments = textarea.value.split('\n');
+    if(rows != undefined && valueSegments.length > rows) { // too many rows
+        newValue = valueSegments.slice(0, rows).join("\n");
+    }
+    if(chars != undefined && textarea.value.length > chars) { // too many chars
+        if(newValue != undefined) 
+            newValue = newValue.substring(0, chars);
+        else
+            newValue = textarea.value.substring(0, chars);
+    }
+    if(newValue != undefined) textarea.value = newValue;
+
+    if(rowsleft_id != undefined) {
+        if(textarea.value == "") document.getElementById(rowsleft_id).innerHTML = rows;
+        else if(rows - valueSegments.length >= 0) document.getElementById(rowsleft_id).innerHTML = rows - valueSegments.length;
+    }
+    if(charsleft_id != undefined) {
+        if(chars != undefined) document.getElementById(charsleft_id).innerHTML = chars - textarea.value.length;
+    }
+}
