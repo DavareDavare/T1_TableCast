d5076ad7eb620a446f152172678c94531f7efbdd
ShizoFX <alessandro.davare@htl-saalfelden.at>
2023-03-02 15:10:29 +0100

    Drehteller.py

--- Flasksite/main.py
+++ Flasksite/main.py
@@ -1,25 +1,66 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, json, escape
 import os
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-@app.route("/", methods=["GET", "POST"])
-def index():
+@app.route("/settings", methods=["GET", "POST"])
+def settings():
     if request.method == "POST":
         helligkeit = request.form["Helligkeit"]
         geschwindigkeit = request.form["Geschwindigkeit"]
         text = request.form["Text"]
+        wlan = request.form["WLAN-Name"]
+        wlanpw = request.form["WLAN-Passwort"]
 
-        #jsonstring = "{'Helligkeit':" + "\'" + helligkeit + "\', " + "\'Geschwindigkeit\':" + "\'" + geschwindigkeit + "\', " + "'Text':" + "\'" + text + "\'}"
-        #data = eval(jsonstring)
+        text = text.replace('\r', ', ').replace('\n', '')
+        stringsss = ["12","321","321"]
+        print(text)
+
+        jsonstring = "{\"Helligkeit\":" + "\"" + helligkeit + "\", " + "\"Geschwindigkeit\":" + "\"" + geschwindigkeit + "\", " + "\"Text\":" + "\"" + text + "\", " + "\"WLAN\":" + "\"" + wlan + "\", ""\"WLAN-Passwort\":" + "\"" + wlanpw + "\"}"
+        data = eval(jsonstring)
+        print(jsonstring)
 
         #print(data['Helligkeit'])
         #print(jsonstring)
 
-        #with open("./Flasksite/inputs.json", "w") as fo:
-            #fo.write(jsonstring)
-        print(text)
-        return f"Geschwindigkeit: {geschwindigkeit} \nHelligkeit:{helligkeit} \n"
+        with open("./Flasksite/inputs.json", "w") as fo:
+            fo.write(jsonstring)
+
+        return f"""
+        <h3>Folgende Werte wurden eingegeben:</h3>
+        <ul>
+            <li>Helligkeit:{escape(helligkeit)}</li>
+            <li>Geschwindigkeit: {escape(geschwindigkeit)}</li>
+            <li>WLAN-Name:{escape(wlan)}</li>
+            <li>WLAN-Passwort:{escape(wlanpw)}</li>
+            <li>Text:</li>
+        </ul>
+        
+        <a href="{"/"}"> Zurück zum Hauptmenü </a>
+        """
+    return render_template("settings.html")
+
+
+@app.route("/load", methods=["GET", "POST"])
+def load():
+    with open("./Flasksite/inputs.json") as json_file:
+        data = json.load(json_file)
+
+    return f"""
+    <H3>Folgende Daten Geladen:</h3> 
+    <ul>
+        <li>Helligkeit:{escape(data['Helligkeit'])}</li>
+        <li>Geschwindigkeit:{escape(data['Geschwindigkeit'])}</li>
+        <li>Wlanname:{escape(data['WLAN'])}</li>
+        <li>Wlanpasswort:{escape(data['WLAN-Passwort'])}</li>
+        <li>Text:{escape(data['Text'])}</li>
+    </ul>
+
+    <a href="{"/"}">Zurück zum Hauptmenü</a>
+    """
+
+@app.route("/")
+def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
